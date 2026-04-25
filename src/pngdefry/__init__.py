30cfed49bd655a10c3d62b0f57626d5989415c0a
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-04-25 14:05:49 -0400

    Add import to __init__.py properly

--- src/pngdefry/__init__.py
+++ src/pngdefry/__init__.py
@@ -1,3 +1,5 @@
-import pngdefry_py.init
-import pngdefry_py.set_flag
-import pngdefry.convert
+from .pngdefry_py import  init
+from .pngdefry_py import  set_flag
+from .pngdefry_py import  convert
+
+__all__ = ["init", "set_flags", "convert"]
